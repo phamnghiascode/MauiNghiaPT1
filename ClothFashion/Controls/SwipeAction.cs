009a79f3247b86f211f5fb0386b377f0a2f7b7b0
nghiapt1 <nghiapt1@newwave.vn>
2024-09-25 15:05:01 +0700

    add some code

---
+++ ClothFashion/Controls/SwipeAction.cs
@@ -0,0 +1,49 @@
+﻿
+namespace ClothFashion.Controls
+{
+    public class SwipeAction : AbsoluteLayout
+    {
+
+        public static readonly BindableProperty ThumbProperty = BindableProperty.Create(
+            nameof(Thumb), typeof(View), typeof(SwipeAction), default(View));
+
+        public View Thumb
+        {
+            get => (View)GetValue(ThumbProperty);
+            set => SetValue(ThumbProperty, value);
+        }
+
+        public static readonly BindableProperty TrackProperty = BindableProperty.Create(
+            nameof(Track), typeof(View), typeof(SwipeAction), default(View));
+
+        public View Track
+        {
+            get => (View)GetValue(TrackProperty);
+            set => SetValue(TrackProperty, value);
+        }
+
+        public SwipeAction()
+        {
+            SizeChanged += OnSizeChanged;
+        }
+
+        void OnSizeChanged(object? sender, EventArgs e)
+        {
+            if (Width == 0 || Height == 0) 
+                return;
+
+            if (Thumb is null || Track is null) 
+                return;
+
+            this.SetLayoutFlags(Track, Microsoft.Maui.Layouts.AbsoluteLayoutFlags.SizeProportional);
+            this.SetLayoutBounds(Track, new Rect(0, 0, 1, 1));
+            if(!Children.Contains(Track))
+                Children.Add(Track);
+
+            this.SetLayoutFlags(Thumb, Microsoft.Maui.Layouts.AbsoluteLayoutFlags.None);
+            this.SetLayoutBounds(Thumb, new Rect(0, 0, Width / 3, Height));
+            if(!Children.Contains(Thumb)) 
+                Children.Add(Thumb);
+        }
+    }
+}
---
+++ ClothFashion/Views/Templates/GalleryDataTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using ClothFashion.Models;
+
+namespace ClothFashion.Views.Templates
+{
+    public class GalleryDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate EvenItemTemplate { get; set; }
+        public DataTemplate OddItemTemplate { get; set; }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            int itemId = ((Item)item).Id;
+
+            if (itemId % 2 == 0)
+                return EvenItemTemplate;
+
+            else return OddItemTemplate;
+        }
+    }
+}
